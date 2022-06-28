0fbb43c5a163bc43357172d895cae7820e9a2507
PeterUIXIV <peter.maier@maierfun.de>
2022-06-28 15:54:09 +0200

    added icons and flex box alignment

--- cvat-ui/src/gamification/components/statistics/statistics-component.tsx
+++ cvat-ui/src/gamification/components/statistics/statistics-component.tsx
@@ -3,14 +3,44 @@
 // SPDX-License-Identifier: MIT
 import React from 'react';
 import 'gamification/gamif-styles.scss';
+import {
+    Row,
+    Col,
+}
+    from 'antd';
 
 interface Props {
     id: number;
+    name?: string;
+    value?: number;
+    unit?: string;
+    hoverOverText?: string;
+    icon?: any;
 }
 
 export default function Statistic(props: Props): JSX.Element {
-    const x = props;
+    const { value, unit, icon } = props;
     return (
-        <>{`Statistic ${x.id}`}</>
+        // <>{`Statistic ${x.id}`}</>
+        <Row>
+            <div className='single-statistic-row'>
+                <Col>
+                    <div className='statistic-icon'>
+                        {/* <Button icon='' type='text'> button </Button> */}
+                        {icon}
+                    </div>
+                </Col>
+                <Col>
+                    <div className='single-statistic-prop'>
+                        {value}
+                    </div>
+                </Col>
+                <Col>
+                    <div className='single-statistic-prop'>
+                        {unit}
+                    </div>
+                </Col>
+            </div>
+        </Row>
     );
 }
--- cvat-ui/src/gamification/components/statistics/statisticslist-component.tsx
+++ cvat-ui/src/gamification/components/statistics/statisticslist-component.tsx
@@ -5,55 +5,100 @@
 import React from 'react';
 import 'gamification/gamif-styles.scss';
 import {
-    Tabs,
     Row,
     Col,
     Collapse,
 }
     from 'antd';
+import { QuestionOutlined } from '@ant-design/icons';
 import Statistic from './statistics-component';
 
 const { Panel } = Collapse;
-const { TabPane } = Tabs;
 
 export default function StatisticsList(): JSX.Element {
     // TODO: Probably do a for-loop for all avaialble statistics -- not sure where to get size yet
+    const icon = <QuestionOutlined style={{ fontSize: '40px' }} />;
+    const iconSmall = <QuestionOutlined style={{ fontSize: '25px' }} />;
+
     return (
         <>
-            <Tabs type='card' defaultActiveKey='1' className='statistics-overview-tabs'>
-                <TabPane tab='Statistics' key='1'>
-                    <Row>
-                        <Col span={4}>
-                            <Statistic id={1} />
-                        </Col>
-                        <Col span={4}>
-                            <Statistic id={2} />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={4}>
-                            <Statistic id={3} />
-                        </Col>
-                        <Col span={4}>
-                            <Statistic id={4} />
-                        </Col>
-                    </Row>
-                    <Collapse bordered={false} defaultActiveKey={['1']}>
+            <div className='statistics-panel'>
+                <Row>
+                    <Col span={12}>
+                        <div className='statistics-col'>
+                            <Statistic id={1} value={156} icon={icon} unit='Images' />
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div className='selected-prop'>
+                            <Statistic id={2} value={14.33} icon={icon} unit='min' />
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12}>
+                        <div className='statistics-col'>
+                            <Statistic id={3} value={8238} icon={icon} />
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div className='statistics-col'>
+                            <Statistic id={4} value={82} icon={icon} unit='hours' />
+                        </div>
+                    </Col>
+                </Row>
+                <Collapse bordered={false} defaultActiveKey={['1']}>
+                    <div className='collapse'>
                         {/* expandIcon */}
-                        <Panel header='This is panel header 1' key='1'>
+                        <Panel header='' key='1'>
+                            {/* <div className='collapse-panel'> */}
                             <Row>
-                                <Col span={4}>
-                                    <Statistic id={5} />
-                                    {/* create 3 * 4 grid with stats */}
+                                <Col span={6}>
+                                    <Statistic id={5} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={6} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={7} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={8} icon={iconSmall} value={24} />
                                 </Col>
                             </Row>
+                            <Row>
+                                <Col span={6}>
+                                    <Statistic id={9} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={10} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={11} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={12} icon={iconSmall} value={24} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={6}>
+                                    <Statistic id={13} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={14} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={15} icon={iconSmall} value={24} />
+                                </Col>
+                                <Col span={6}>
+                                    <Statistic id={16} icon={iconSmall} value={24} />
+                                </Col>
+                            </Row>
+                            {/* </div> */}
                         </Panel>
-                    </Collapse>
-                </TabPane>
-                <TabPane tab='Challenges' key='2'>
-                    Content of Tab Pane 2
-                </TabPane>
-            </Tabs>
+                    </div>
+                </Collapse>
+            </div>
         </>
     );
 }
