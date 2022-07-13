3c8578eb98d02418f2859cd6927d32f12186930f
PeterUIXIV <peter.maier@maierfun.de>
2022-07-13 10:25:15 +0200

    arranged quick statistics

--- cvat-ui/src/components/annotation-page/top-bar/quick-statistics.tsx
+++ cvat-ui/src/components/annotation-page/top-bar/quick-statistics.tsx
@@ -5,19 +5,22 @@ import React from 'react';
 import { Row, Col } from 'antd/lib/grid';
 import QuickStatistic from 'gamification/components/statistics/quick-statistics-component';
 import ProgressBar from 'gamification/components/progressbar-component';
+import { QuestionOutlined } from '@ant-design/icons';
 
 function QuickStatisticsPanel(): JSX.Element {
+    const iconSmall = <QuestionOutlined style={{ fontSize: '25px' }} />;
+
     return (
         <div className='cvat-annotation-header-quick-statistics'>
             <Row>
                 <Col>
-                    <QuickStatistic id={1} />
+                    <QuickStatistic id={1} value={22} icon={iconSmall} unit='hrs' />
                 </Col>
                 <Col>
-                    <QuickStatistic id={2} />
+                    <QuickStatistic id={2} value={24} icon={iconSmall} />
                 </Col>
                 <Col>
-                    <QuickStatistic id={3} />
+                    <QuickStatistic id={3} value={14} icon={iconSmall} unit='min' />
                 </Col>
             </Row>
             <Row>
--- cvat-ui/src/gamification/components/statistics/quick-statistics-component.tsx
+++ cvat-ui/src/gamification/components/statistics/quick-statistics-component.tsx
@@ -3,17 +3,44 @@
 // SPDX-License-Identifier: MIT
 import 'gamification/gamif-styles.scss';
 import React from 'react';
+import {
+    Row,
+    Col,
+}
+    from 'antd';
 
 interface Props {
     id: number;
+    name?: string;
+    value?: number;
+    unit?: string;
+    hoverOverText?: string;
+    icon?: any;
 }
 
 export default function QuickStatistic(props: Props): JSX.Element {
-    const { id } = props;
+    const { icon, value, unit } = props;
 
     return (
-        <div>
-            {`Statistic ${id}`}
-        </div>
+        <Row>
+            <div className='single-quick-statistic-component'>
+                <Col>
+                    <div className='statistic-icon'>
+                        {/* <Button icon='' type='text'> button </Button> */}
+                        {icon}
+                    </div>
+                </Col>
+                <Col>
+                    <div className='single-statistic-prop'>
+                        {value}
+                    </div>
+                </Col>
+                <Col>
+                    <div className='single-statistic-prop'>
+                        {unit}
+                    </div>
+                </Col>
+            </div>
+        </Row>
     );
 }
