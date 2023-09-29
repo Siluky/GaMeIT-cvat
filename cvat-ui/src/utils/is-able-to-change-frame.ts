0a44fb822d2417b5c30cd0dc5165a7eca762b682
Waschbaerkebab <nikolai.betz@web.de>
2023-09-29 12:30:08 +0200

    No SWITCH_FRAME Actions when playing Energizer

--- cvat-ui/src/utils/is-able-to-change-frame.ts
+++ cvat-ui/src/utils/is-able-to-change-frame.ts
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 import { getCVATStore } from 'cvat-store';
+import { EnergizerType } from 'gamification/gamif-interfaces';
 import { CombinedState } from 'reducers/interfaces';
 
 export default function isAbleToChangeFrame(): boolean {
@@ -10,6 +11,15 @@ export default function isAbleToChangeFrame(): boolean {
 
     const state: CombinedState = store.getState();
     const { instance } = state.annotation.canvas;
+    const { activeEnergizer } = state.energizer; // To check energizer state
+
+    // Debug Code
+    console.log(`State of the activeEnergizer: ${activeEnergizer}`);
+    console.log(`EnergizerType== None? : ${activeEnergizer === EnergizerType.NONE}`);
+
     return !!instance && instance.isAbleToChangeFrame() &&
-        !state.annotation.player.navigationBlocked;
+        !state.annotation.player.navigationBlocked &&
+        // From here on additional conditions to prevent Frame-Changes when playing an Energizer-Game
+        !!activeEnergizer &&
+        activeEnergizer === EnergizerType.NONE;
 }
