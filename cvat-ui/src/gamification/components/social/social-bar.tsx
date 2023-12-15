23678f9b2e43b98cf22b4056721db8498429a43d
Waschbaerkebab <nikolai.betz@web.de>
2023-12-15 12:00:40 +0100

    removed unneccessary onClick()

--- cvat-ui/src/gamification/components/social/social-bar.tsx
+++ cvat-ui/src/gamification/components/social/social-bar.tsx
@@ -8,7 +8,7 @@ import {
     CloseOutlined, LeftOutlined, RightOutlined, UserOutlined,
 } from '@ant-design/icons';
 import { getCVATStore } from 'cvat-store';
-import { getFriendsListAsync, toggleChatVisibility, toggleChatWindow } from 'gamification/actions/social-actions';
+import { getFriendsListAsync, toggleChatWindow } from 'gamification/actions/social-actions';
 import { OnlineStatus, Profile } from 'gamification/gamif-interfaces';
 import { addGamifLog } from 'gamification/actions/user-data-actions';
 import { connect, useDispatch } from 'react-redux';
@@ -53,9 +53,6 @@ const chatBar = (friend?: Profile): JSX.Element => {
             key={friend.userId}
             // FIXME: make this into a method
             className={(friend.sentAMessage && status === OnlineStatus.ONLINE) ? 'gamif-chat-bar-bubble unreadMessage' : 'gamif-chat-bar-bubble'}
-            onClick={() => {
-                dispatch(toggleChatVisibility(friend.userId, !friend.chatActive));
-            }}
             // FIXME: Prevent Button from changing colors when hovering/clicking
             // + problem with clicking near edge and moving out
         >
